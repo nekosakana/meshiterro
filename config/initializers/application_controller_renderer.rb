ea4cd86747d7cda0fb3fbf4f7987273fb2a7a917
nekosakana <kiri33pudding@gmail.com>
2020-05-23 18:28:13 +0900

    [Finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'bb973c1436805b76057136982acbf85ae8feb926fcf9dcee3cfb35138e674c248887e7ac03a5e7bdfa9df66bc428b8bc94a76511c052b6b8807810881249f1ae'
