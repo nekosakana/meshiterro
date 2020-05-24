86b55a99da228ed9e37aca8316b5db5a410a2bc5
nekosakana <kiri33pudding@gmail.com>
2020-05-24 14:01:03 +0900

    [Start]5-5

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :post_images, only:[:new,:create,:index,:show] do
   	resources :post_comments, only:[:create, :destroy]
 end
-	resources :post_images, only: [:new, :create, :index, :show] do
+	resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
 end
